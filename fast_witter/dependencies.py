302b0864f75dba91e638169dcf55cc08f208c7fe
Mark <marko4148@gmail.com>
2022-05-23 22:44:26 +0300

    Added PaginationQueryParams class

--- fast_witter/dependencies.py
+++ fast_witter/dependencies.py
@@ -7,3 +7,9 @@ def get_db() -> BlogSession:
         yield db
     finally:
         db.close()
+
+
+class PaginationQueryParams:
+    def __init__(self, offset: int = 0, limit: int = 100) -> None:
+        self.offset = offset
+        self.limit = limit
