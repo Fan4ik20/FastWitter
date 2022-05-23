85c3b7d6418d7c8cd831a8f872452689362ffaca
Mark <marko4148@gmail.com>
2022-05-23 22:57:48 +0300

    Registered posts, users_posts routers

--- fast_witter/main.py
+++ fast_witter/main.py
@@ -4,11 +4,15 @@ import uvicorn
 import config
 
 from routers import users
+from routers import posts
+from routers import users_posts
 
 
 app = FastAPI(docs_url='/api/v1/docs/')
-app.include_router(users.router, prefix='/api/v1')
 
+app.include_router(users.router, prefix='/api/v1')
+app.include_router(posts.router, prefix='/api/v1')
+app.include_router(users_posts.router, prefix='/api/v1')
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=65432)
