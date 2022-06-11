5baef07e1f7af0f256a9eaf82a6779ddb7eee53e
Mark <marko4148@gmail.com>
2022-06-12 01:03:34 +0300

    Added few methods for serving post_comments router. Added DbInterface with creating tables method

--- fast_witter/database/interfaces.py
+++ fast_witter/database/interfaces.py
@@ -1,9 +1,15 @@
-from sqlalchemy import select, delete
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from database import models
+from database import models, settings
 import schemas
 from security import service
+
+
+class DbInterface:
+    @staticmethod
+    def create_tables() -> None:
+        models.BlogBase.metadata.create_all(bind=settings.blog_engine)
 
 
 class UserInterface:
@@ -126,6 +132,27 @@ class CommentInterface:
         ).all()
 
     @staticmethod
+    def get_user_post_comments(
+            db: Session, user_id: int, post_id: int,
+            offset: int = 0, limit: int = 100
+    ) -> list[models.Comment]:
+        return db.scalars(
+            select(models.Post).filter_by(
+                user_id=user_id, post_id=post_id
+            ).offset(offset).limit(limit)
+        ).all()
+
+    @staticmethod
+    def get_user_post_comment(
+            db: Session, user_id: int, post_id: int, comment_id: int
+    ) -> models.Comment | None:
+        return db.execute(
+            select(models.Comment).filter_by(
+                id=comment_id, user_id=user_id, post_id=post_id
+            )
+        ).scalar_one_or_none()
+
+    @staticmethod
     def get_user_comment(
             db: Session, user_id: int, comment_id: int
     ) -> models.Comment | None:
@@ -133,8 +160,24 @@ class CommentInterface:
         return db.execute(
             select(models.Comment).filter_by(
                 id=comment_id, user_id=user_id
-            ).scalar_one_or_none()
+            )
+        ).scalar_one_or_none()
+
+    @staticmethod
+    def create_comment(
+            db: Session, user_id: int, post_id: int,
+            comment: schemas.CommentCreate
+    ) -> models.Comment:
+        new_comment = models.Comment(
+            user_id=user_id, post_id=post_id, **comment.dict()
         )
+
+        db.add(new_comment)
+        db.commit()
+
+        db.refresh(new_comment)
+
+        return new_comment
 
     @staticmethod
     def get_comment(db: Session, comment_id) -> models.Comment | None:
