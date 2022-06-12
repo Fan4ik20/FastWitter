7faa92e80c58353814c86b125ce42d31e965c672
Mark <marko4148@gmail.com>
2022-06-12 17:42:46 +0300

    Added more checking in user creating. Added users/me endpoing

--- fast_witter/config.py
+++ fast_witter/config.py
@@ -1,3 +1,20 @@
+import os
+
+from pydantic import BaseModel
+
 from dotenv import load_dotenv
 
+from fastapi_jwt_auth import AuthJWT
+
 load_dotenv('.env')
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+
+class Settings(BaseModel):
+    authjwt_secret_key: str
+
+
+@AuthJWT.load_config
+def get_config():
+    return Settings(authjwt_secret_key=SECRET_KEY)
--- fast_witter/routers/users.py
+++ fast_witter/routers/users.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter, Depends, status
+
+from fastapi_jwt_auth import AuthJWT
+
 from sqlalchemy.orm import Session
 
 import schemas
@@ -30,8 +33,23 @@ def get_users(
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     if UserInterface.get_user_by_email(db, user.email):
         raise exc.ObjectWithGivenAttrAlreadyExist('User',  'Email')
+    if UserInterface.get_user_by_username(db, user.username):
+        raise exc.ObjectWithGivenAttrAlreadyExist('User', 'Username')
 
     return UserInterface.create_user(db, user)
+
+
+@router.get('/me/', response_model=schemas.User)
+def get_current_user(
+        authorize: AuthJWT = Depends(), db: Session = Depends(get_db)
+):
+    authorize.jwt_required()
+
+    current_username = authorize.get_jwt_subject()
+
+    user = UserInterface.get_user_by_username(db, current_username)
+
+    return user
 
 
 @router.get('/{user_id}/', response_model=schemas.User)
