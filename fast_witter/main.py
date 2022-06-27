c1bc332827b8e69aa0b8e82fa943f1bd6be7606e
Mark <marko4148@gmail.com>
2022-06-27 23:55:19 +0300

    Renamed some handlesr. Registered new router. Registered new exc

--- fast_witter/main.py
+++ fast_witter/main.py
@@ -7,8 +7,10 @@ import uvicorn
 
 import config
 
-from routers import users
 from routers import auth
+
+from routers.users import users
+from routers.users import user_followers
 
 from routers.posts import user_posts
 from routers.posts import posts
@@ -27,6 +29,7 @@ DbInterface.create_tables()
 
 app.include_router(auth.router, prefix='/api/v1')
 app.include_router(users.router, prefix='/api/v1')
+app.include_router(user_followers.router, prefix='/api/v1')
 app.include_router(posts.router, prefix='/api/v1')
 app.include_router(user_posts.router, prefix='/api/v1')
 app.include_router(comments.router, prefix='/api/v1')
@@ -42,20 +45,22 @@ def request_object_not_found_handler(
         status_code=status.HTTP_404_NOT_FOUND,
         content={
             'message':
-                f'Requested {exc_.model} with given identifier not found'
+                f'Requested {exc_.model} with given identifier not found',
+            'place': 'path'
         }
     )
 
 
 @app.exception_handler(exc.ObjectWithGivenAttrAlreadyExist)
-def user_already_registered_handler(
+def object_with_given_attr_exist_handler(
         request: Request, exc_: exc.ObjectWithGivenAttrAlreadyExist
 ):
     return JSONResponse(
         status_code=status.HTTP_409_CONFLICT,
         content={
             'message':
-                f'{exc_.model} with given {exc_.conflict_attr} already exist'
+                f'{exc_.model} with given {exc_.conflict_attr} already exist',
+            'place': 'body'
         }
     )
 
@@ -65,6 +70,14 @@ def not_object_owner_handler(request: Request, exc_: exc.NotObjectOwner):
     return JSONResponse(
         status_code=status.HTTP_403_FORBIDDEN,
         content={'message': f'You are the not owner of this {exc_.model}'}
+    )
+
+
+@app.exception_handler(exc.CantPerformThis)
+def cant_do_it_again_handler(request: Request, exc_: exc.CantPerformThis):
+    return JSONResponse(
+        status_code=status.HTTP_409_CONFLICT,
+        content={'message': exc_.msg}
     )
 
 
