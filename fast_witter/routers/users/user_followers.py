e2455ace753a78c6f91e8ea3f5654a4ce3b3eb50
Mark <marko4148@gmail.com>
2022-06-27 23:54:27 +0300

    Added followers/following funnctionallity

---
+++ fast_witter/routers/users/user_followers.py
@@ -0,0 +1,89 @@
+from fastapi import APIRouter, Depends
+
+from sqlalchemy.orm import Session
+
+from schemas import user_schemas as schemas
+
+import exc
+
+from database.interfaces.user_interface import UserInterface
+from database.models import User
+
+from dependencies import get_db, PaginationQueryParams, get_active_user
+
+router = APIRouter(
+    prefix='/users/{user_id}', tags=['User Followers|Following']
+)
+
+
+def get_user_or_raise_exc(user_id: int, db: Session) -> User:
+    user = UserInterface.get_user(db, user_id)
+
+    if user is None:
+        raise exc.RequestedObjectNotFound('User')
+
+    return user
+
+
+@router.get('/followers/', response_model=list[schemas.User])
+def get_user_followers(
+        user_id: int, db: Session = Depends(get_db),
+        pagination_params: PaginationQueryParams = Depends()
+):
+    get_user_or_raise_exc(user_id, db)
+
+    user_followers = UserInterface.get_user_followers(
+        db, user_id, pagination_params.offset, pagination_params.limit
+    )
+
+    return user_followers
+
+
+@router.get('/following/', response_model=list[schemas.User])
+def get_user_following(
+        user_id: int, db: Session = Depends(get_db),
+        pagination_params: PaginationQueryParams = Depends()
+):
+    get_user_or_raise_exc(user_id, db)
+
+    following = UserInterface.get_user_following(
+        db, user_id, pagination_params.offset, pagination_params.limit
+    )
+
+    return following
+
+
+@router.post('/follow/')
+def follow_user(
+        user_id: int, db: Session = Depends(get_db),
+        active_user: User = Depends(get_active_user)
+):
+    followed = get_user_or_raise_exc(user_id, db)
+
+    if user_id == active_user.id:
+        raise exc.CantPerformThis('You can\'t follow yourself')
+
+    if UserInterface.is_user_followed(db, user_id, active_user.id):
+        raise exc.CantPerformThis('You can\'t follow again')
+
+    UserInterface.follow_user(db, followed, active_user)
+
+    return {'status': 'OK'}
+
+
+@router.post('/unfollow/')
+def unfollow_user(
+        user_id: int, db: Session = Depends(get_db),
+        active_user: User = Depends(get_active_user)
+):
+    followed = get_user_or_raise_exc(user_id, db)
+
+    if user_id == active_user.id:
+        raise exc.CantPerformThis('You can\'t unfollow yourself')
+
+    if not UserInterface.is_user_followed(db, user_id, active_user.id):
+        raise exc.CantPerformThis('You can\'t unfollow without being followed')
+
+    UserInterface.unfollow_user(db, followed, active_user)
+
+    return {'status': 'OK'}
